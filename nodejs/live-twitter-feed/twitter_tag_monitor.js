2c42a28359c06b048fd92e0e4f921570203e5bfc
Sean Oh <ohsiwon@gmail.com>
2013-11-04 16:27:14 -0500

    oops

--- nodejs/live-twitter-feed/twitter_tag_monitor.js
+++ nodejs/live-twitter-feed/twitter_tag_monitor.js
@@ -9,8 +9,8 @@ var twit = new twitter({
 });
 
 
-var $tags = ['#c3xdev', '#test_tag'];
-// var $tags = ['food'];
+// var $tags = ['#c3xdev', '#test_tag'];
+var $tags = ['food'];
 var $valid_tags = [];
 
 console.log('--> Twitter hashtag monitor start: ', $tags);
@@ -36,7 +36,7 @@ function streamListener(stream){
           // console.log('Content: ' + data['text']);
           // console.log('Image: ' + data['entities']['media'][0].media_url);
           // console.log(util.inspect(data));
-          postData(data);
+          // postData(data);
    
           //Send data through socket
           io.sockets.in('room').emit('new_twit', data['entities']['media'][0].media_url);
