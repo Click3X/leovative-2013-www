fe7b0d373896ef4fcb4687fbd0262046161b6c71
Sean Oh <ohsiwon@gmail.com>
2013-11-04 15:59:53 -0500

    progress

--- nodejs/live-twitter-feed/twitter_tag_monitor.js
+++ nodejs/live-twitter-feed/twitter_tag_monitor.js
@@ -8,8 +8,8 @@ var twit = new twitter({
   access_token_secret: 'zu9vOlkp0DAjRg1W3G6NfyPnaoNs4JeB8Ag3n6E5cQs51'
 });
 
-// var $tags = ['#c3xdev'];
-var $tags = ['food'];
+var $tags = ['#c3xdev', '#test_tag'];
+// var $tags = ['food'];
 var $valid_tags = [];
 
 console.log('--> Twitter hashtag monitor start: ', $tags);
@@ -25,17 +25,23 @@ function streamListener(stream){
     //Post only if tweet has photo attached
     if(data['entities'] != null){
       if(data['entities']['media'] != undefined){
-        $valid_tags = getTags(data['text']);
-        console.log('-------------------------');
-        console.log('New tweet has photo with tag ' + '#'+ $valid_tags + ' is here -- ' + data['created_at']);
-        // console.log('Screen name: ' + data['user']['screen_name']);
-        // console.log('Content: ' + data['text']);
-        // console.log('Image: ' + data['entities']['media'][0].media_url);
-        // console.log(util.inspect(data));
-        // postData(data);
- 
-        //Send data through socket
-        io.sockets.in('room').emit('new_twit', data['entities']['media'][0].media_url);
+        
+
+        // $valid_tags = getTags(data['text']);      
+
+        if(getTags(data['text'])){
+
+          console.log('-------------------------');
+          console.log('New tweet has photo with tag ' + $tags.toString() + ' is here -- ' + data['created_at']);
+          // console.log('Screen name: ' + data['user']['screen_name']);
+          // console.log('Content: ' + data['text']);
+          // console.log('Image: ' + data['entities']['media'][0].media_url);
+          // console.log(util.inspect(data));
+          // postData(data);
+   
+          //Send data through socket
+          // io.sockets.in('room').emit('new_twit', data['entities']['media'][0].media_url);
+        }
       }
     }
     else{
@@ -52,22 +58,30 @@ function streamListener(stream){
 
 //Workers
 function getTags(str){
+
+  $tag_num = 0;
+
   $words = str.split(' ');
-  $valid_tags = [];
+  // $valid_tags = [];
 
   $tags.forEach(function(tag,i){
     $words.forEach(function(w,j){
+      // console.log(tag + ' == ' + w);
       if(tag == w){
-        $valid_tags.push(tag.replace('#',''));
+        // $valid_tags.push(tag.replace('#',''));
+        $tag_num++;
       }
     });
 
   });
-  return $valid_tags.toString();
+
+  return ($tag_num >= $tags.length);
+
+  // return $valid_tags.toString();
 };
 
 function postData(data){
-  var _parameter = '?tag=' + $valid_tags + '&image_url=' + escape(data['entities']['media'][0].media_url) + '&screen_name=' + data['user']['screen_name'];
+  var _parameter = '?tag=leovative' + '&image_url=' + escape(data['entities']['media'][0].media_url) + '&screen_name=' + data['user']['screen_name'];
 
   console.log(_parameter);
   http
